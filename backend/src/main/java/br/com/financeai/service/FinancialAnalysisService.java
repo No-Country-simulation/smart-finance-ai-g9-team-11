dfa1afa5dfa09910c99ced303d17bd4b820fb20d
Leilanny Rodrigues <leilannyrodrigues@gmail.com>
2026-07-15 18:32:12 -0300

    feat: implementação do MlRequest

--- backend/src/main/java/br/com/financeai/service/FinancialAnalysisService.java
+++ backend/src/main/java/br/com/financeai/service/FinancialAnalysisService.java
@@ -3,6 +3,7 @@ package br.com.financeai.service;
 import br.com.financeai.dto.request.FinancialAnalysisRequest;
 import br.com.financeai.dto.response.FinancialAnalysisResponse;
 import br.com.financeai.integration.client.MlServiceClient;
+import br.com.financeai.integration.dto.MlRequest;
 import br.com.financeai.integration.dto.MlResponse;
 import org.springframework.stereotype.Service;
 
@@ -15,17 +16,24 @@ public class FinancialAnalysisService {
         this.mlServiceClient = mlServiceClient;
     }
 
-    public FinancialAnalysisResponse analyze(FinancialAnalysisRequest request){
+    public FinancialAnalysisResponse analyze(FinancialAnalysisRequest request) {
+        // 1. Criamos o seu MlRequest pegando os dados limpos que vieram do controller
+        MlRequest mlRequest = new MlRequest(
+                request.rendaMensal(),
+                request.nivelEndividamento(),
+                request.frequenciaPoupanca(),
+                request.transacoes()
+        );
 
-        MlResponse mlResponse = mlServiceClient.analyze(request);
+        // 2. Agora passamos o mlRequest (que o client espera receber)
+        MlResponse mlResponse = mlServiceClient.analyze(mlRequest);
 
         return new FinancialAnalysisResponse(
                 mlResponse.perfilFinanceiro(),
                 mlResponse.probabilidade(),
                 mlResponse.resumoGastos(),
                 mlResponse.recomendacoes()
-
         );
-
     }
+
 }
